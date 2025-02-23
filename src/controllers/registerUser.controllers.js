3d898f1d84be74114ab8d3c6fae29b97d4af8a49
DMad19 <dineshmadduru98813@gmail.com>
2025-02-23 23:12:35 +0530

    third commit

--- src/controllers/registerUser.controllers.js
+++ src/controllers/registerUser.controllers.js
@@ -336,6 +336,77 @@ const updateUserCoverPic = asyncHandler(async (req, res) => {
     res.status(200).json(200, user, "COver Image updated successfully");
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if (!username.trim()) {
+        throw new APIError(404, "Username not found");
+    }
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase(),
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subcriber",
+                as: "subscribedTo",
+            },
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers",
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo",
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                avatar: 1,
+                subscribersCount: 1,
+                subscribedToCount: 1,
+                isSubscribed: 1,
+                coverimage: 1,
+                email: 1,
+            },
+        },
+    ]);
+    if (!channel?.length) {
+        throw new APIError(404, "Channel Not Found");
+    }
+    return res
+        .status(200)
+        .json(
+            new APIResponse(
+                200,
+                channel[0],
+                "channel profile fetched successfully"
+            )
+        );
+});
+
 export {
     registerUser,
     loginUser,
@@ -346,4 +417,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverPic,
+    getUserChannelProfile,
 };
